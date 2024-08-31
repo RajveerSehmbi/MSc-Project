2de6d5f5ffc43e62f76a9b6caf73a38a3f0d9555
RajveerSehmbi <rajveer.sehmbi@outlook.com>
2024-08-31 20:26:24 +0100

    Added print statements

--- Data/Pathways/trimpathway.py
+++ Data/Pathways/trimpathway.py
@@ -11,6 +11,7 @@ engine = create_engine(f"sqlite:///{variables.database_path}")
 
 # Read pathway_genes.csv
 pathway_genes = pd.read_csv(variables.pathway_file)
+print(pathway_genes.shape)
 
 # Read train data and keep columns
 train_columns = []
@@ -21,10 +22,12 @@ for i in range(0, 46):
     df = df.columns
     # Save list of ciolumn names
     train_columns.extend(df)
+    print(len(train_columns))
     print(f"Read train_{i}")
 
 # Get the gene column from the pathway_genes.csv
 all_genes = pathway_genes['gene']
+print(all_genes.shape)
 
 # Get the genes that are not common between the train data and the pathway_genes.csv
 gene_no_data = all_genes[~all_genes.isin(train_columns)]
@@ -32,6 +35,7 @@ print(gene_no_data)
 
 # Remove the gene_no_data genes from the pathway_genes.csv
 pathway_genes = pathway_genes[~pathway_genes['gene'].isin(gene_no_data)]
+print(pathway_genes.shape)
 
 # Save the trimmed pathway_genes.csv
 pathway_genes.to_csv(f"{variables.pathway_file}_trimmed", index=False)
