b54eaefdc3d0f9745770a8d8e4cbcd10e50d1d36
RajveerSehmbi <rajveer.sehmbi@outlook.com>
2024-09-11 12:56:20 +0100

    bash script for gpu cluster

---
+++ Data/SQLite/dae_transform.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=gpgpuC
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=rs218
+#SBATCH --output=final%j.out
+
+export PATH=/vol/bitbucket/${USER}/fullenv/bin/:$PATH
+
+source activate
+source /vol/cuda/12.0.0/setup.sh
+uptime
+
+python3 /vol/bitbucket/rs218/MSc-Project/Data/SQLite/DAE_transform.py
+
+/usr/bin/nvidia-smi
